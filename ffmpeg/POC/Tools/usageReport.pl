8a10a6cc7a8cc80cb38ec24c82d5ffaeb2e61afd
Randy Marlow <rmarlow@sensysnetworks.com>
2014-06-23 14:13:21 -0700

    working version

--- ffmpeg/POC/Tools/usageReport.pl
+++ ffmpeg/POC/Tools/usageReport.pl
@@ -2,7 +2,7 @@
 # $Id$
 
 # if no arguments, pring usage
-#@ARGV || &usage;
+@ARGV || &usage;
 
 use strict;
 use warnings;
@@ -18,7 +18,6 @@ my $searchString = undef;
 my $DEBUG = 0;
 my $VERBOSE = 0;
 my %image2script = ();
-my @DIRSTACK = ();
 
 select(STDOUT); $| = 1;     # make unbuffered
 
@@ -46,18 +45,23 @@ while (@ARGV) {
     };
 }
 
+if (! defined $searchString) {
+    print "!!! ERROR: no search string defined\n";
+    &usage;
+}
+
 #========================================================================
 # Main loop
 #========================================================================
 
+# assume that this is run in the current working dir
 my $dir = '.';
-
 
 # get all *.config files in current dir on down
 opendir (DIR, $dir) or die $!;
-while (my $file = readdir(DIR)) {
-    $file =~ /\.config$/ || next;
-    print "... Processing: ", $file, "\n";
+while (my $configFile = readdir(DIR)) {
+    $configFile =~ /\.config$/ || next;
+    print "... Processing: ", $configFile, "\n"if $VERBOSE;
 
     # parse configfile
     my $imageFile = undef;
@@ -65,7 +69,7 @@ while (my $file = readdir(DIR)) {
     my $lineno = 0;
     my $READ_SCRIPT = 0;
 
-    open (CFGFILE, "$file") || die $!;
+    open (CFGFILE, "$configFile") || die $!;
     while (my $line = <CFGFILE>) {
 	$lineno++;
 	# this matches the image entries
@@ -82,20 +86,24 @@ while (my $file = readdir(DIR)) {
 	};
 	# default: add text to imageFile's script
 	if (defined $imageFile) {
-	    push @{$image2script{$imageFile}}, $line;
+	    # concatenate config file and image file
+	    # into one key
+	    my $key = $configFile . ':' . $imageFile;
+	    push @{$image2script{$key}}, $line;
 	}
     }
     close CFGFILE;
 }
 close DIR;
 
-# now look for the search string
-for my $imageFile (sort keys %image2script) {
-    if ($imageFile =~ /$searchString/) {
-	print "### $imageFile:\n";
-	for my $line (@{$image2script{$imageFile}}) {
+# now look for the search string across all the files
+for my $key (sort keys %image2script) {
+    if ($key =~ /$searchString/) {
+	print "@@@ $key:\n";
+	for my $line (@{$image2script{$key}}) {
 	    print $line;
 	}
+	print "\n";
     }
 }
 
@@ -109,13 +117,23 @@ sub usage {
 
     print<<"EOF";
 
-USAGE: $0 [options] <config-file>
+USAGE: $0 [options]
 
   Option:                 Description:
   ---------               ----------------------------------------------
-  -d(ebug)                # set DEBUG flag
-  -h(elp)?                # displays usage
-  -v(erbose)              # show more output from ffmpeg
+  -find '<search-string>' # find <search-string> in all *.config files
+  -verbose                # print more feedback
+
+
+Output looks like:
+
+@@@ <config-file-name>:<image-file>:
+<script>
+
+e.g.:
+
+@@@ Intro_to_APM2.config:../Screenshots/Intro_to_APM/Slide19.jpg:
+#AppDynamics provides deep visibility into your production systems so that you can drill down quickly to the source of the problem, even in complex, distributed#We hope that this short video was informative to you and made you excited about implementing the tool to manage your applications.
 
 EOF
 
@@ -123,45 +141,5 @@ EOF
 
 } # end: usage
 
-#---------------------------------------------------------------------
-sub pushd {
-#---------------------------------------------------------------------
-
-    my($dir) = shift;
-    my($cwd) = '';
-
-    -d "$dir"
-        || die "!!! ERROR: pushd: $dir doesn't exist";
-
-    chop($cwd = `pwd`);
-    push(@DIRSTACK, "$cwd");
-
-    chdir "$dir"
-        || die "!!! ERROR: pushd: Can't pushd $dir";
-
-    #return the full dir path
-    chop($cwd = `pwd`);
-    print ">>> Changing dir to $cwd\n";
-    return "$cwd";
-
-} # end: pushd
-
-#---------------------------------------------------------------------
-sub popd {
-#---------------------------------------------------------------------
-
-    @DIRSTACK
-        || die "!!! ERROR: popd: Directory stack empty";
-
-    my($dir) = pop(@DIRSTACK);
-
-    -d "$dir"
-        || die "!!! ERROR: popd: $dir doesn't exist";
-    chdir "$dir"
-        || die "!!! ERROR: popd: Can't popd $dir";
-
-    return "$dir";
-
-} # end: popd
 
 
