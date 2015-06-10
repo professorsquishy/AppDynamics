5d3263a76e85d1f4b137c576db6e55983aac454f
Randy Marlow <rmarlow@sensysnetworks.com>
2015-06-10 15:35:22 -0700

    added

---
+++ ffmpeg/Tools/searchAndReplace.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import sys
+import os.path
+import csv
+import re
+import fileinput
+
+searchReplaceFile = 'old_and_new_paths.csv'
+fileToSearch = sys.argv[1]
+
+# read in the search/replace hash
+reader = csv.reader(open(searchReplaceFile, 'r'))
+replacements = dict(reader)
+#for src, target in replacements.iteritems():
+#    if not os.path.isfile(target):
+#        print("File: " + target + " not found")
+
+
+# iterate over the replacements
+for line in fileinput.input(fileToSearch, inplace=True):
+    for src, target in replacements.iteritems():
+        line = line.replace(src, target)
+    print(line, end='')
+
